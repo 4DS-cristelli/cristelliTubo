ce09b0af3d054958b2163bf3ed3062fbc3d176f3
Eric Cristelli <eric.cristelli@buonarroti.tn.it>
2023-09-21 18:36:51 +0200

    feat: aggiunto nel main un ciclo do while per inizializzare l'oggetto tubo

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,50 @@ public class Main {
         Scanner input;
         input = new Scanner(System.in);
 
+        Tubo tubo1 = null;
         //variabili tmp
-        float raggio;
-        float altezza;
+        float raggio= 0;
+        float altezza = 0;
         boolean flag;
 
-        do{//ciclo input selezione con eccezzioni
-            try{
-                input = new Scanner(System.in);
-                System.out.print("Inserisci il raggio del tubo:  ");
-                raggio = input.nextByte();
-                flag = true;
-            }catch(Exception e){
-                System.out.println(e.getMessage());
-                flag = false;
-            }
-        }while(!flag);
 
-        do{//ciclo input selezione con eccezzioni
-            try{
-                input = new Scanner(System.in);
-                System.out.print("Inserisci l'altezza del tubo:  ");
-                altezza = input.nextByte();
+        do{
+            do{//ciclo input selezione con eccezzioni
+                try{
+                    input = new Scanner(System.in);
+                    System.out.print("Inserisci il raggio del tubo:  ");
+                    raggio = input.nextByte();
+                    flag = true;
+                }catch(Exception e){
+                    System.out.println(e.getMessage());
+                    flag = false;
+                }
+            }while(!flag);
+
+            do{//ciclo input selezione con eccezzioni
+                try{
+                    input = new Scanner(System.in);
+                    System.out.print("Inserisci l'altezza del tubo:  ");
+                    altezza = input.nextByte();
+                    flag = true;
+                }catch(Exception e){
+                    System.out.println(e.getMessage());
+                    flag = false;
+                }
+            }while(!flag);
+
+            try {
+                tubo1 = new Tubo(raggio, altezza);
                 flag = true;
-            }catch(Exception e){
-                System.out.println(e.getMessage());
+            }catch (Exception e){
+                System.out.println(e);
                 flag = false;
             }
-        }while(!flag);
+        }while (!flag);
+
+
+
+
 
 
         System.out.println("Hello world!");
