e401f9ca9f4c568402da7c80b479138815cc48fa
eric.cristelli <eric.cristelli@buonarroti.tn.it>
2023-09-21 14:35:09 +0200

    feat: aggiunto metodo costruttore

--- src/Tubo.java
+++ src/Tubo.java
@@ -1,4 +1,15 @@
 public class Tubo {
     private float raggio;
     private float altezza;
+
+    public Tubo(float r, float a)throws Exception{
+        if(r >0 && a >0){
+            this.raggio = r;
+            this.altezza = a;
+        }else{
+            throw new Exception("Parametri errati");
+        }
+    }
 }
+
+
